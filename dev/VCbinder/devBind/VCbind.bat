d6c4d15cf91abf8686b70a3e891b1091bcdf3c51
Dennis Hamilton <orcmid@apache.org>
2016-11-15 11:08:51 -0800

    0.0.9 VCbind.bat cleanups
    Description of parameters and smoothing of nomenclature and messages

--- dev/VCbinder/devBind/VCbind.bat
+++ dev/VCbinder/devBind/VCbind.bat
@@ -1,5 +1,5 @@
 @echo off
-rem VCbind.zip\VCbind.bat 0.0.8       UTF-8                     dh:2016-11-14
+rem VCbind.zip\VCbind.bat 0.0.9       UTF-8                     dh:2016-11-15
 rem -----1---------2---------3---------4---------5---------6---------7-------*
 
 rem                  SETTING VC++ COMMAND-SHELL ENVIRONMENT
@@ -14,9 +14,6 @@ rem accompanying VCbind.txt file.  For further information, see
 rem <http://nfoWare.com/dev/2016/11/d161101.htm> and check for the latest
 rem version at <http://nfoWare.com/dev/2016/11/d161101b.htm>.
 
-rem This file reflects ideas applied at the Apache Software Foundation for
-rem <https://archive.apache.org/dist/openoffice/4.1.2-patch1/binaries/Windows>
-
 rem ANNOUNCE THIS SCRIPT
 rem     XXX: Assume Stand-alone operation for now.
 rem          For nesting in another script, find a way to be more "headless"
@@ -28,7 +25,7 @@ rem   Soft white background with blue text
 
 CLS
 ECHO:
-ECHO: [VCbind] 0.0.8 SETTING UP VC++ COMMAND-SHELL ENVIRONMENT
+ECHO: [VCbind] 0.0.9 SETTING UP VC++ COMMAND-SHELL ENVIRONMENT
 ECHO:          %TIME% %DATE% on %USERNAME%'s %COMPUTERNAME% 
 rem VERIFY MINIMUM OPERATING CONDITIONS
 IF NOT CMDEXTVERSION 2 GOTO :FAIL0
@@ -42,10 +39,33 @@ IF NOT EXIST "%~dp0VCbind.txt" GOTO :FAIL1
 IF NOT EXIST "%~dp0VCbind.bat" GOTO :FAIL1
 
 rem DETERMINE PARAMETERS
+rem    VCbind.bat has the following command-line parameters:
+
+rem         > VCbind [*][option [toolset]]
+
+rem    where
+rem             * signifies that non-verbose output is preferred
+rem               (suitable when operated from within another script)
+rem        config is one of the VC++ "platform" cases:
+rem                        x86 for producing x86 code via the x86 compiler
+rem                      amd64 for producing x64 code via the x64 compiler
+rem                  amd64_x86 for producing x86 code via the x64 compiler
+rem                  x86_amd64 for producing x64 code via the x86 compiler
+rem               Some toolsets and platforms will limite these.
+rem               x86 is the default and always works.
+rem       toolset identifies which common tools to start checking from:
+rem                        140 for Visual Studio 2015 (14.0) flavors, then 
+rem                        120 for Visual Studio 2013 (12.0) flavors, then
+rem                        110 for Visual Studio 2012 (11.0) flavors, then
+rem                        100 for Visual Studio 2010 (10.0) flavors, and then
+rem                         90 for Visual Studio 2008 (9.0) flavors
+rem                140 is the default  
+rem                XXX: This variability allows for easy testing also.
+
 rem    Three environment variables are set whenever there is a successful
 rem    VCbind.bat conclusion:
-rem           VCbound is the environment (e.g., 140) that was used
-rem       VCmodeBound is the mode (e.g., x86) of compilation bound
+rem           VCbound identifes the common tools (e.g., 140) that were used
+rem     VCboundConfig is the bound configuration (e.g., x86) for compilation
 rem        VCboundVer is the version (e.g., 14.0) of Visual Studio
 rem                   that the VC++ build tools correspond to.
 rem    other VC* environment-variable names are used transiently and will
@@ -54,19 +74,23 @@ rem    be altered without checking whether they are already defined.
 rem    XXX: For now, assume no VC* parameters are requested on the
 rem         VCbind.bat command-line.
 SET VCasked=%VCbound%
-SET VCmodeAsked=x86
+SET VCaskedConfig=x86
 
 rem CHECK WHETHER VCBIND SETTINGS HAVE ALREADY BEEN MADE
 IF DEFINED VCbound GOTO :ALREADY
 
 rem CHECK WHETHER THERE HAS BEEN SOME OTHER BINDING ALREADY 
-IF DEFINED VisualStudioVersion GOTO :FAIL4
+IF DEFINED VCINSTALLDIR GOTO :FAIL4
 
 rem FIND LATEST-AVAILABLE RELEASED VC++ BUILD TOOLS
 :TRY140
 IF NOT DEFINED VS140COMNTOOLS GOTO :TRY120
+set VisualStudioVersion=14.0
+rem    XXX: Because Build Tools case doesn't set it
 CALL :VCTRY "%VS140COMNTOOLS%..\..\VC\" 140
 IF NOT ERRORLEVEL 9 EXIT /B %ERRORLEVEL%
+set VisualStudioVersion=
+rem    XXX: Because guessed wrong
 
 :TRY120
 IF NOT DEFINED VS120COMNTOOLS GOTO :TRY110
@@ -100,17 +124,17 @@ rem            0 is returned if there was a reported SUCCESS case
 set ERRORLEVEL=
 set VCasked=%2%
 IF NOT EXIST %1\vcvarsall.bat EXIT /B 9
-CALL %1\vcvarsall.bat %VCmodeAsked%
-IF ERRORLEVEL 1 EXIT /B 2
+CALL %1\vcvarsall.bat %VCaskedConfig%
+IF ERRORLEVEL 1 GOTO :FAIL5
 
 :WINNER
-ECHO:          Success: VC++ %VisualStudioVersion% %VCmodeAsked%-mode set up.
+ECHO:          Success: VC++ %VisualStudioVersion% %VCaskedConfig% config set.
 GOTO :SUCCESS
 
 :ALREADY
 rem CHECK IF THERE IS A CONFLICT WITH THE PREVIOUS BINDING
 IF NOT "%VCasked%" == "%VCbound%" GOTO :FAIL2
-IF NOT "%VCmodeAsked%" == "%VCmodeBound%" GOTO :FAIL2
+IF NOT "%VCaskedConfig%" == "%VCboundConfig%" GOTO :FAIL2
 
 rem CHECK IF ANOTHER CONFLICT HAS ARISEN
 IF NOT "%VCboundVer%" == "%VisualStudioVersion%" GOTO :FAIL3
@@ -118,39 +142,36 @@ IF NOT "%VCboundVer%" == "%VisualStudioVersion%" GOTO :FAIL3
 rem TRUST PREVIOUS SETTINGS TO BE REUSABLE
 rem      Avoid multiple running of vcvarsall and duplicating the PATH and
 rem      other parameter settings.
-ECHO:          Using existing VC++ %VCboundVer% %VCmodeBound%-mode settings.        
+ECHO:          Using existing VC++ %VCboundVer% %VCboundConfig% config setup.        
 GOTO :SUCCESS
 
 :SUCCESS
-rem    XXX: Assuming no need to fiddle ERRORLEVEL at this point.
 ECHO:
 ECHO:
 SET VCbound=%VCasked%
-SET VCmodeBound=%VCmodeAsked%
+SET VCboundConfig=%VCaskedConfig%
 SET VCboundVer=%VisualStudioVersion%
-rem    correct whether first or subsequent success
+rem    appropriate whether or not already set
 PAUSE
 EXIT /B 0
 
 :FAIL6
 ECHO:          *** NO VC++ BUILD TOOLS FOUND ***
-ECHO:          VC++ desktop build tools could not be located.
+ECHO:          VCbind-supported desktop build tools could not be located.
 ECHO:
 ECHO:          NO ENVIRONMENT CHANGES HAVE BEEN MADE
-ECHO:          VCbind does not check earlier than Visual Studio 2008.
 ECHO:          See %<http://nfoWare.com/dev/2016/11/d161101.htm%>
 ECHO:          for suitable freely-available versions.
 GOTO :BAIL
 
 :FAIL5
+set VisualStudioVersion=
+rem     XXX: Incase incorrectly guessed in a :TRYnnn step.
 ECHO:
-ECHO: [VCbind] *** SETUP FOR TOOLS %VCasked% %VCmodeAsked%-MODE FAILED ***
-ECHO:          Check preceding messag(s)for failure information.
+ECHO: [VCbind] *** SETUP FOR TOOLS %VCasked% %VCaskedConfig%-CONFIG FAILED ***
+ECHO:          Check preceding messag(s) for failure information.
 ECHO:
-ECHO:          THE CURRENT STATE IS UNPREDICTABLE
-ECHO:          It is likely that %VCmodeAsked%-mode is not supported by
-ECHO:          the installed VS %VCasked% Common Tools.  There may
-ECHO:          be other problems.
+ECHO:          NO VC++ CONFIGURATION ENVIRONMENT IS SET
 GOTO :BAIL
 
 :FAIL4
@@ -169,9 +190,8 @@ GOTO :NOMIXING
 :FAIL2
 ECHO:          **** CONFLICT WITH A PRIOR VCBIND ****
 ECHO:          The current request conflicts with settings already
-ECHO:          in effect for %VCmodeBound%-mode compilations using the
+ECHO:          in effect for %VCboundConfig% config compilations using the
 ECHO:          VC++ compiler of Visual Studio version %VisualStudioVersion%.
-rem               TODO: confirm version is always set when vcvarsall works.
 GOTO :NOMIXING
 
 :NOMIXING
@@ -203,7 +223,7 @@ ECHO:
 ECHO:          NO CHANGES HAVE BEEN MADE
 ECHO:          To enable Command Extensions, arrange to initiate
 ECHO:          the command shell with the /E:ON command-line option
-ECHO:          before performing VCbind.bat.
+ECHO:          before VCbind.bat is performed directly or indirectly.
 GOTO :BAIL
 
 :BAIL
@@ -218,21 +238,26 @@ EXIT /B %ERRORLEVEL%
 rem -----1---------2---------3---------4---------5---------6---------7-------*
 
 rem                    Copyright 2006 Dennis E. Hamilton
-rem
+
+rem This file reflects ideas applied at the Apache Software Foundation for
+rem <https://archive.apache.org/dist/openoffice/4.1.2-patch1/binaries/Windows>
+
 rem Licensed under the Apache License, Version 2.0 (the "License");
 rem you may not use this file except in compliance with the License.
 rem You may obtain a copy of the License at
-rem
-rem     http://www.apache.org/licenses/LICENSE-2.0
-rem
+rem     <http://www.apache.org/licenses/LICENSE-2.0>
+
 rem Unless required by applicable law or agreed to in writing, software
 rem distributed under the License is distributed on an "AS IS" BASIS,
 rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem See the License for the specific language governing permissions and
 rem limitations under the License.
 
+
 rem -----1---------2---------3---------4---------5---------6---------7-------*
 
+rem 0.0.9 2016-11-15-11:03 Define parameters, choose "config" naming of
+rem       platform types, and smooth messages and comments some more.
 rem 0.0.8 2016-11-14-17:43 Add Try ladder for determining the latest version
 rem       installed.  Simplify message layout further.
 rem 0.0.7 2016-11-14-09:55 Smooth output of messages
