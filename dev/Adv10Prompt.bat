19df5028cb038496e515cc39f502a436f8b91cc7
Dennis E. Hamilton <dennis.hamilton@acm.org>
2024-10-05 14:57:52 -0700

    Adv10 Splash temporary hack

---
+++ dev/Adv10Prompt.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Adv10Prompt.bat 0.0.0           UTF-8                         2024-10-05
+rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+rem
+rem                    SETTING PLACEHOLDER Adv10 PROMPT
+rem
+rem    This batch file sets up a default prompt to black and then reverts to
+rem    the default colors of the CMD window.  This brings emphasis to the
+rem    prompt and makes it easier to see where user entries are made.
+rem
+rem    To see how this works, type PROMPT /? at the command prompt.
+
+PROMPT $E[30m$S$S$S$S$S$G$E[0m
+
+rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+rem
+rem 0.0.0  2024-10-05T16:14Z Fork of MyCleanC myPrompt.bat 0.0.0
+rem
+rem                    *** end of Adv10Prompt.bat ***
