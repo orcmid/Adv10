a8945fd502bf33500ab52f01aeb6e2375f4f8e27
Dennis E. Hamilton <dennis.hamilton@acm.org>
2024-10-06 13:23:37 -0700

    0.0.2 Leave entries after the ">" in black

--- dev/Adv10Splash.bat
+++ dev/Adv10Splash.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Adv10Splash.bat 0.0.1           UTF-8                         2024-10-06
+REM Adv10Splash.bat 0.0.2           UTF-8                         2024-10-06
 rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 rem
 rem                      SIMPLE SPLASH DEMO FOR ADV10
@@ -14,12 +14,15 @@ ECHO:        building.  Around you is a forest.  A small stream flows out
 ECHO:        of the building and down a gully.
 ECHO:
 ECHO:        Welcome to Adventure!!  Would you like instructions?
-PROMPT $E[30m$S$S$S$S$S$S$G$S$E[0m
+PROMPT $E[30m$S$S$S$S$S$S$G$S
 rem    This places the Adv10 prompt indented under the splash screen "[Adv10]"
-rem    To see how this prompt setup works, see the batch file Adv10Prompt.bat
-
+rem    NOTE: The $E0m needs to be at the start of messages to reset to default
+rem    And the game does this a little differently.  Maybe we need a bold
+rem    black for the users responses.
+rem
 rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 rem
+rem 0.0.2  2024-10-06T20:21Z Leave the black setting for user's response.
 rem 0.0.1  2024-10-06T18:16Z Remove " after "instructions?""
 rem 0.0.0  2024-10-05T18:56Z Fork of Adv10Prompt.bat 0.0.0
 rem
