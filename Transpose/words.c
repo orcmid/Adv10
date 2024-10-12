3e366bb8847efebb51ac8119952c73791b3d2f4e
Dennis E. Hamilton <dennis.hamilton@acm.org>
2024-10-12 14:23:43 -0700

    0.0.0 words Placeholders for Vocabulary Database module

---
+++ Transpose/words.c
@@ -0,0 +1,28 @@
+/* words.c 0.0.0                      UTF-8                     dh:2024-10-11
+ *
+ *                           ADVENTURE VERSION 1.0
+ *                           =====================
+ *
+ *                THE ORIGINAL COLLOSAL CAVE TEXT ADVENTURE
+ *
+ *                         VOCABULARY DATABASE MODULE
+ *                         --------------------------
+ *
+ *    Copyright © 1998 by Don Woods and Don Knuth; all rights reserved.
+ *    Adapted with permission.
+ *
+ *    Derived by Dennis E. Hamilton from the 2010-01-26 version of Don
+ *    Knuth's Literate Programming source code file, advent.w.gz, available
+ *    at <http://www-cs-faculty.stanford.edu/~knuth/programs.html#advent>.
+ *    This version incorporates all applicable errata published through
+ *    January 2022-04-21 for Knuth's "Selected Papers on Fun and Games,"
+ *    <http://www-cs-faculty.stanford.edu/~knuth/fg.html>, identified as
+ *    [fg] below.
+ */
+
+
+/*
+   0.00 2024-10-11T20:36Z Start the vocabulary database module.
+   */
+
+/*                        ***** end of words.c *****                      */
---
+++ Transpose/words.h
@@ -0,0 +1,27 @@
+/* words.h 0.0.0                      UTF-8                     dh:2024-10-11
+ *
+ *                           ADVENTURE VERSION 1.0
+ *                           =====================
+ *
+ *                THE ORIGINAL COLLOSAL CAVE TEXT ADVENTURE
+ *
+ *                          VOCABULARY DATABASE API
+ *                          -----------------------
+ *
+ *    Copyright © 1998 by Don Woods and Don Knuth; all rights reserved.
+ *    Adapted by Dennis E. Hamilton with permission.
+ *
+ *    The Vocabulary Database contains forms of all of the words that are
+ *    recognized by the Adventure program.  In the current version, the
+ *    words are fixed and the vocabulary is not extensible.
+ *
+ *    The vocabulary is in English language and the words consist of ASCII
+ *    characters.  Case is ignored. Traditionally, words longer than 5 letters
+ *    are reduced to the first 5 letters in the database.
+ */
+
+
+/*  0.0.0 2024-10-11T21:02Z Start the vocabulary database API.
+   */
+
+/*                        ***** end of words.h *****                      */
