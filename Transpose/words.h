d5c3720882e6c95d37063c506dc043a1a1e5a324
Dennis E. Hamilton <dennis.hamilton@acm.org>
2024-10-14 09:40:05 -0700

    0.0.3 words.h touch-up, introduce motion entries typedef #9

--- Transpose/words.h
+++ Transpose/words.h
@@ -1,4 +1,4 @@
-/* words.h 0.0.2                      UTF-8                     dh:2024-10-13
+/* words.h 0.0.3                      UTF-8                     dh:2024-10-14
  *
  *                           ADVENTURE VERSION 1.0
  *                           =====================
@@ -32,11 +32,17 @@ typedef enum {      no_type,   // 0, and not a word type
 
 /*      Each word in the vocabulary database has a wordtype specified in
         its database entry.
-           no_type is defined in [fg:27.5] but not used.
-           Advent exploits no_type for determining when a slot in the
+
+        no_type is defined in [fg:27.5] but not used.
+
+        Advent exploits no_type values for determining when a slot in the
         vocabulary in empty.
-           Adv10 also uses no_type when there is no room to add an entry
-        and when a word is not in the vocabulary.
+
+        Adv10 returns a special no_type entry when the dictionary has no room
+        for an additional entry.
+
+        XXX: Exhausting the capacity of the dictionary is not supposed to
+             happen.  The Advent code does not check for this condition.
         */
 
 typedef struct {    unsigned char text[6];   // sz string of up to 5 letters
@@ -45,16 +51,40 @@ typedef struct {    unsigned char text[6];   // sz string of up to 5 letters
 
                     } wordentry;    // [fg:27.5]
 
-/*         The meaning value is one of the enumeration values associated with
+/*      The meaning entry is one of the enumeration values associated with
         the given word_type.
-           Advent uses the type name hashentry for this structure, saying
-        more about the implementation than is needed for the API.
-           For Adv10, the term wordentry is chosen to be more about purpose.
+
+        Advent uses the type name "hashentry" for this structure.  For Adv10,
+        the term "wordentry" places focus on purpose above the API.
+
         Unsigned char types are used to avoid possible difficulties in the
-        unlikely case that any of the values exceeds 127.
+        currently-unlikely case that any of the values exceeds 127.
         */
 
+/* MOTION_TYPE TERMS */
+
+typedef enum { N, S, E, W, NE, SE, NW, SW, U, D, L, R, IN, OUT, FORWARD, BACK,
+               OVER, ACROSS, UPSTREAM, DOWNSTREAM, ENTER, CRAWL, JUMP, CLIMB,
+               LOOK, CROSS, ROAD, WOODS, VALLEY, HOUSE, GULLY, STREAM,
+               DEPRESSION, ENTRANCE, CAVE, ROCK, SLAB, BED, PASSAGE, CAVERN,
+               CANYON, AWKWARD, SECRET, BEDQUILT, RESERVOIR, GIANT, ORIENTAL,
+               SHELL, BARREN, BROKEN, DEBRIS, VIEW, FORK, PIT, SLIT, CRACK,
+               DOME, HOLE, WALL, HALL, ROOM, FLOOR, STAIRS, STEPS, COBBLES,
+               SURFACE, DARK, LOW, OUTDOORS, Y2, XYZZY, PLUGH, PLOVER, OFFICE,
+               NOWHERE
+
+               } motion;    // [fg:27.9]
+
+/*         For a wordentry with word_type motion_type, the meaning entry will
+           be one of these term values.
+
+           Note that there are both directions and places among the motion
+           terms.  These provide for a kind of fast-travel to places of the
+           game, once visited or learned.
+           */
+
 /*
+   0.0.3  2024-10-14T16:37Z Touch-up, add motion entries typedef
    0.0.2  2024-10-13T21:36Z Introduce wordtype and wordentry typedefs
    0.0.1  2024-10-13T16:32Z Add synopsis
    0.0.0  2024-10-11T21:02Z Start the vocabulary database API.
